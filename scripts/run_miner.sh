511a85ec407d830692e979161751b1eabe584be9
Dmytro Savenkov <dmytro.savenkov@chain-insights.ai>
2024-12-15 20:27:11 +0200

    Scripts with debug

--- scripts/run_miner.sh
+++ scripts/run_miner.sh
@@ -9,6 +9,8 @@ cp -r env venv_miner/
 export PYTHONPATH=$(pwd)
 echo "PYTHONPATH is set to $PYTHONPATH"
 NETWORK_TYPE=${1:-mainnet}
+NETWORK_TYPE=${1:-mainnet}
+echo "NETWORK_TYPE is set to $NETWORK_TYPE"
 cd src
 python3 subnet/miner/miner.py $NETWORK_TYPE
 
--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -9,6 +9,7 @@ cp -r env venv_validator/
 export PYTHONPATH=$(pwd)
 echo "PYTHONPATH is set to $PYTHONPATH"
 NETWORK_TYPE=${1:-mainnet}
+echo "NETWORK_TYPE is set to $NETWORK_TYPE"
 cd src
 python3 subnet/cli.py $NETWORK_TYPE
 
