29eabf8a87e4f611a15d622a326bb519e039b83b
Dmytro Savenkov <dmytro.savenkov@chain-insights.ai>
2024-12-20 15:00:56 +0200

    Two databases PG are supported now

--- migrations/settings.py
+++
@@ -1,6 +0,0 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
-
-
-class MigrationSettings(BaseSettings):
-    DATABASE_URL: str
-    model_config = SettingsConfigDict(extra='allow')
---
+++ src/subnet/validator/database/migrations/settings.py
@@ -0,0 +1,27 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
+import os
+
+
+def load_environment(env: str = "mainnet"):
+    """Load the appropriate .env file based on the environment."""
+    if env == 'mainnet':
+        dotenv_path = os.path.abspath('../../../../env/.env.validator.mainnet')
+    elif env == 'testnet':
+        dotenv_path = os.path.abspath('../../../../env/.env.validator.testnet')
+    else:
+        raise ValueError(f"Unknown environment: {env}")
+
+    if os.path.exists(dotenv_path):
+        load_dotenv(dotenv_path=dotenv_path)
+    else:
+        raise FileNotFoundError(f"Environment file not found: {dotenv_path}")
+
+
+# Load the default environment
+load_environment()
+
+
+class ValidatorMigrationSettings(BaseSettings):
+    DATABASE_URL: str
+    model_config = SettingsConfigDict(extra='allow')
